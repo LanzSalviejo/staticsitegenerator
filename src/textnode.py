bcc4b11b8b064f04a9a49f639d32e64ff38d8861
LanzSalviejo <lanzsalviejo@gmail.com>
2024-08-16 21:49:21 -0700

    Added TextNode class, .gitignore, public directory, main.sh, and main.py

---
+++ src/textnode.py
@@ -0,0 +1,17 @@
+class TextNode:
+    def __init__(self, text, text_type, url = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+    
+    def __eq__(self, other):
+        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+def main():
+    test = TextNode("This is a text node", "bold", "https://www.boot.dev")
+    print(test)
+
+main()
